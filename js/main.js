5fc3ad797f393315c163af28a29ee7c23b173fd1
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-11 13:56:24 -0800

    When a button is clicked, stores value into variable

--- js/main.js
+++ js/main.js
@@ -1,21 +1,51 @@
-function add(x, y){
-    return x + y
+//Create operators for four buttons, add , subtract, multiply, divide
+//allow these operators to be called from function of operate
+//When clicking a number, place number into the input of the num1 in operate function 
+//Once an operator button is pressed begin operator function, switch to num2
+//gather input for second number
+//when equal is pressed, preform function 
+
+
+function add(x, y) {
+  return x + y;
 }
 
-function subtract(x, y){
-    return x - y
+function subtract(x, y) {
+  return x - y;
 }
 
-function multiply(x, y){
-    return x * y
+function multiply(x, y) {
+  return x * y;
 }
 
-function divide(x, y){
-    return x / y
+function divide(x, y) {
+  return x / y;
 }
 
-function operate(func, n1, n2){
-    return func === add ? add(n1, n2) : func === subtract ? subtract(n1, n2) : func === multiply ? multiply(n1, n2) : func === divide ? divide(n1, n2) : 'error'
+function operate(func, n1, n2) {
+  return func === add
+    ? add(n1, n2)
+    : func === subtract
+    ? subtract(n1, n2)
+    : func === multiply
+    ? multiply(n1, n2)
+    : func === divide
+    ? divide(n1, n2)
+    : "error";
 }
 
-console.log(operate(divide, 5, 4))
+let num1 = 0;
+
+document.querySelectorAll(".btn").forEach((item) => {
+  item.addEventListener("click", getData)
+})
+
+function getData(event) {
+    num1 += event.target.textContent
+    console.log(num1)
+};
+
+
+
+
+
