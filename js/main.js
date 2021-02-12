d981c1b6e3ce919b9a07b27494a866efca80100e
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-12 13:51:25 -0800

    formatted code

--- js/main.js
+++ js/main.js
@@ -1,94 +1,117 @@
 //Create operators for four buttons, add , subtract, multiply, divide
 //allow these operators to be called from function of operate
-//When clicking a number, place number into the input of the num1 in operate function
-//Once an operator button is pressed begin operator function, switch to num2
+//When clicking a number, place number into the input of the firstInput in operate function
+//Once an operator button is pressed begin operator function, switch to secondInput
 //gather input for second number
 //when equal is pressed, preform function
 
 function add(x, y) {
-   outputValue.textContent = x + y
+  sum === undefined ? (sum = x + y) : (sum += x + y);
+  outputValue.textContent = sum;
+  clear();
 }
 
 function subtract(x, y) {
-  outputValue.textContent = x - y
+  sum === undefined ? (sum = x - y) : (sum += x - y);
+  outputValue.textContent = sum;
+  clear();
 }
 
 function multiply(x, y) {
-  outputValue.textContent = x * y
+  outputValue.textContent = x * y;
+  sum === undefined ? (sum = x * y) : (sum += x * y);
+  clear();
 }
 
 function divide(x, y) {
-  outputValue.textContent = x / y
+  outputValue.textContent = x / y;
+  sum === undefined ? (sum = x / y) : (sum += x / y);
+  clear();
 }
 
 function operate(func, n1, n2) {
-  return func === '+'
+  return func === "+"
     ? add(n1, n2)
-    : func === '-'
+    : func === "-"
     ? subtract(n1, n2)
-    : func === '×'
+    : func === "×"
     ? multiply(n1, n2)
-    : func === '÷'
+    : func === "÷"
     ? divide(n1, n2)
     : "error";
 }
 
+function clear() {
+  firstInput = undefined;
+  secondInput = undefined;
+  operating = undefined;
+  sum = undefined;
+}
+
+//Number inputs
 let firstInput;
 let secondInput;
+//operator input
 let operating;
+//sum input.
+let sum;
 
-let outputValue = document.querySelector('h2');
-
-// document.querySelectorAll(".btn").forEach((item) => {
-//   item.addEventListener("click", getData)
-// })
-
-// function getData(event) {
-//   if (operating === undefined){
-//     num1 += event.target.textContent
-//     console.log(num1)
-//   }
-// };
+let outputValue = document.querySelector("h2");
 
 //Gets numbers from clicking on number buttons/
 document.querySelectorAll(".number").forEach((item) => {
-  item.addEventListener("click", getData);
+  item.addEventListener("click", getOperation);
 });
 
 //if the operating number has not been clicked, placed button value into firstInput, otherwise put it into secondInput
-function getData(event) {
+function getOperation(event) {
   if (operating === undefined) {
-    //if the first input equals undefined 
+    //if the first input equals undefined, make firstInput equal to button press, otherwise add to firstInput value
     firstInput === undefined
       ? (firstInput = event.target.textContent)
       : (firstInput += event.target.textContent);
     console.log(firstInput);
-    outputValue.innerText = firstInput
+    outputValue.innerText = firstInput;
+    //If the operating value has been clicked, then start adding to the second value
   } else if (operating !== undefined) {
     secondInput === undefined
       ? (secondInput = event.target.textContent)
       : (secondInput += event.target.textContent);
     console.log(secondInput);
-    outputValue.innerText = secondInput
-  } else {
-
+    outputValue.innerText = secondInput;
   }
 }
 
-//Event Listener for clicking on operators, 
+//Event Listener for clicking on operators, gets operator for use
 document.querySelectorAll(".operator").forEach((item) => {
   item.addEventListener("click", getOp);
 });
 
 function getOp(event) {
-  if(firstInput !== undefined){
+  if (firstInput !== undefined) {
     operating = event.target.textContent;
   }
   console.log(operating);
 }
 
-document.querySelector(".equals").addEventListener("click", solveEquation, false)
+document
+  .querySelector(".equals")
+  .addEventListener("click", solveEquation, false);
 
-function solveEquation(){
-  operate(operating, Number(firstInput), Number(secondInput))
+function solveEquation() {
+  if (sum !== undefined) {
+    operate(Number(sum), Number(firstInput), Number(secondInput));
+  } else {
+    operate(operating, Number(firstInput), Number(secondInput));
+  }
+}
+
+document.querySelector("#clear").addEventListener("click", clearAll, false);
+
+function clearAll() {
+  firstInput = undefined;
+  secondInput = undefined;
+  operating = undefined;
+  sum = undefined;
+  outputValue.innerText = 0;
 }
