7e5207ef16c982b71a00a34057bfcad544ec1901
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-15 10:30:45 -0800

    refactored code to add class for calculator, remade codebase

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 //gather input for second number
 //when equal is pressed, preform function
 
+/*
 function add(x, y) {
   sum === undefined ? (sum = x + y) : (sum += x + y);
   outputValue.textContent = sum;
@@ -115,3 +116,129 @@ function clearAll() {
   sum = undefined;
   outputValue.innerText = 0;
 }
+*/
+
+class Calculator {
+  constructor(firstInput, secondInput, operator) {
+    (this.firstInput = firstInput),
+      (this.secondInput = secondInput),
+      (this.operator = operator);
+  }
+
+  add() {
+    sum === undefined
+      ? (sum = this.firstInput + this.secondInput)
+      : (sum = sum + this.secondInput);
+    output.innerText = sum
+    clear()
+
+  }
+  subtract() {
+    sum === undefined
+      ? (sum = this.firstInput - this.secondInput)
+      : (sum = sum - this.secondInput);
+      output.innerText = sum
+      clear()
+
+  }
+  multiply() {
+    sum === undefined
+      ? (sum = this.firstInput * this.secondInput)
+      : (sum = sum * this.secondInput);
+      output.innerText = sum
+      clear()
+
+  }
+  divide() {
+    sum === undefined
+      ? (sum = this.firstInput / this.secondInput)
+      : (sum = sum / this.secondInput);
+      output.innerText = sum
+      clear()
+  }
+
+  operate() {
+    this.operator === "+"
+      ? this.add()
+      : this.operator === "-"
+      ? this.subtract()
+      : this.operator === "×"
+      ? this.multiply()
+      : this.operator === "÷"
+      ? this.divide()
+      : console.log("ERR");
+  }
+
+}
+document.querySelector('#clear').addEventListener('click', clearAll)
+
+function clearAll(){
+  inputOne = undefined
+  inputTwo = undefined
+  operatorSymbol = undefined
+  sum = undefined
+  output.innerText = 0
+}
+
+function clear(){
+  inputOne = undefined
+  inputTwo = undefined
+  operatorSymbol = undefined
+}
+
+let output = document.querySelector('h2')
+output.innerText = 0
+
+document.querySelectorAll(".number").forEach((item) => {
+  item.addEventListener("click", getData);
+});
+//get variables, undefined to start.
+let inputOne;
+let inputTwo;
+let operatorSymbol;
+let sum;
+
+//get the ipput variables if they are undefined, first check if the operatorSymbol is undefined so we can get the first input
+function getData(event) {
+  if (operatorSymbol === undefined) {
+    inputOne === undefined
+      ? (inputOne = event.target.textContent)
+      : (inputOne += event.target.textContent);
+      output.innerText = inputOne
+    console.log(inputOne);
+  } else if (operatorSymbol !== undefined) {
+    inputTwo === undefined
+      ? (inputTwo = event.target.textContent)
+      : (inputTwo += event.target.textContent);
+      output.innerText = inputTwo
+    console.log(inputTwo);
+  }
+}
+
+document.querySelectorAll(".operator").forEach((item) => {
+  item.addEventListener("click", getOperator);
+});
+
+function getOperator(event) {
+  if (inputOne !== undefined) {
+    operatorSymbol = event.target.textContent;
+  } else if (operatorSymbol === undefined){
+    operatorSymbol = event.target.textContent;
+
+  }
+  console.log(operatorSymbol);
+}
+
+document.querySelector(".equals").addEventListener("click", operating);
+
+function operating() {
+  if (sum === undefined) {
+    let calc = new Calculator(Number(inputOne), Number(inputTwo), operatorSymbol);
+    calc.operate();
+  } else if (sum !== undefined){
+    let calc = new Calculator(Number(sum), Number(inputTwo), operatorSymbol)
+    calc.operate()
+  }
+}
+
+
