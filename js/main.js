48d564c308c9c85012aad53511f9cc4e14723059
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-16 09:51:30 -0800

    deleted unused code, formatted and cleaned code

--- js/main.js
+++ js/main.js
@@ -5,119 +5,6 @@
 //gather input for second number
 //when equal is pressed, preform function
 
-/*
-function add(x, y) {
-  sum === undefined ? (sum = x + y) : (sum += x + y);
-  outputValue.textContent = sum;
-  clear();
-}
-
-function subtract(x, y) {
-  sum === undefined ? (sum = x - y) : (sum += x - y);
-  outputValue.textContent = sum;
-  clear();
-}
-
-function multiply(x, y) {
-  outputValue.textContent = x * y;
-  sum === undefined ? (sum = x * y) : (sum += x * y);
-  clear();
-}
-
-function divide(x, y) {
-  outputValue.textContent = x / y;
-  sum === undefined ? (sum = x / y) : (sum += x / y);
-  clear();
-}
-
-function operate(func, n1, n2) {
-  return func === "+"
-    ? add(n1, n2)
-    : func === "-"
-    ? subtract(n1, n2)
-    : func === "×"
-    ? multiply(n1, n2)
-    : func === "÷"
-    ? divide(n1, n2)
-    : "error";
-}
-
-function clear() {
-  firstInput = undefined;
-  secondInput = undefined;
-  operating = undefined;
-  sum = undefined;
-}
-
-//Number inputs
-let firstInput;
-let secondInput;
-//operator input
-let operating;
-//sum input.
-let sum;
-
-let outputValue = document.querySelector("h2");
-
-//Gets numbers from clicking on number buttons/
-document.querySelectorAll(".number").forEach((item) => {
-  item.addEventListener("click", getOperation);
-});
-
-//if the operating number has not been clicked, placed button value into firstInput, otherwise put it into secondInput
-function getOperation(event) {
-  if (operating === undefined) {
-    //if the first input equals undefined, make firstInput equal to button press, otherwise add to firstInput value
-    firstInput === undefined
-      ? (firstInput = event.target.textContent)
-      : (firstInput += event.target.textContent);
-    console.log(firstInput);
-    outputValue.innerText = firstInput;
-    //If the operating value has been clicked, then start adding to the second value
-  } else if (operating !== undefined) {
-    secondInput === undefined
-      ? (secondInput = event.target.textContent)
-      : (secondInput += event.target.textContent);
-    console.log(secondInput);
-    outputValue.innerText = secondInput;
-  }
-}
-
-//Event Listener for clicking on operators, gets operator for use
-document.querySelectorAll(".operator").forEach((item) => {
-  item.addEventListener("click", getOp);
-});
-
-function getOp(event) {
-  if (firstInput !== undefined) {
-    operating = event.target.textContent;
-  }
-  console.log(operating);
-}
-
-document
-  .querySelector(".equals")
-  .addEventListener("click", solveEquation, false);
-
-function solveEquation() {
-  if (sum !== undefined) {
-    operate(Number(sum), Number(firstInput), Number(secondInput));
-  } else {
-    operate(operating, Number(firstInput), Number(secondInput));
-  }
-}
-
-document.querySelector("#clear").addEventListener("click", clearAll, false);
-
-function clearAll() {
-  firstInput = undefined;
-  secondInput = undefined;
-  operating = undefined;
-  sum = undefined;
-  outputValue.innerText = 0;
-}
-*/
-
 class Calculator {
   constructor(firstInput, secondInput, operator) {
     (this.firstInput = firstInput),
@@ -129,38 +16,35 @@ class Calculator {
     sum === undefined
       ? (sum = this.firstInput + this.secondInput)
       : (sum = sum + this.secondInput);
-    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000
-    output.innerText = sum
-    clear()
-
+    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000;
+    output.innerText = sum;
+    clear();
   }
   subtract() {
     sum === undefined
       ? (sum = this.firstInput - this.secondInput)
       : (sum = sum - this.secondInput);
-    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000
-    output.innerText = sum
-    clear()
-
+    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000;
+    output.innerText = sum;
+    clear();
   }
   multiply() {
     sum === undefined
       ? (sum = this.firstInput * this.secondInput)
       : (sum = sum * this.secondInput);
-    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000   
-    output.innerText = sum
-    clear()
-
+    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000;
+    output.innerText = sum;
+    clear();
   }
   divide() {
     sum === undefined
       ? (sum = this.firstInput / this.secondInput)
       : (sum = sum / this.secondInput);
-      sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000      
-      output.innerText = sum
-      clear()
+    sum = Math.round((sum + Number.EPSILON) * 10000000) / 10000000;
+    output.innerText = sum;
+    clear();
   }
-// does operation based on operatorSymbol 
+  // does operation based on operatorSymbol
   operate() {
     this.operator === "+"
       ? this.add()
@@ -172,28 +56,27 @@ class Calculator {
       ? this.divide()
       : console.log("ERR");
   }
-
 }
-document.querySelector('#clear').addEventListener('click', clearAll)
+document.querySelector("#clear").addEventListener("click", clearAll);
 
 //clears all variables to start fresh
-function clearAll(){
-  inputOne = undefined
-  inputTwo = undefined
-  operatorSymbol = undefined
-  sum = undefined
-  output.innerText = 0
+function clearAll() {
+  inputOne = undefined;
+  inputTwo = undefined;
+  operatorSymbol = undefined;
+  sum = undefined;
+  output.innerText = 0;
 }
 
-//clears operating variables 
-function clear(){
-  inputOne = undefined
-  inputTwo = undefined
-  operatorSymbol = undefined
+//clears operating variables
+function clear() {
+  inputOne = undefined;
+  inputTwo = undefined;
+  operatorSymbol = undefined;
 }
 
-let output = document.querySelector('h2')
-output.innerText = 0
+let output = document.querySelector("h2");
+output.innerText = 0;
 
 document.querySelectorAll(".number").forEach((item) => {
   item.addEventListener("click", getData);
@@ -211,18 +94,16 @@ function getData(event) {
       ? (inputOne = event.target.textContent)
       : inputOne.length < 10
       ? (inputOne += event.target.textContent)
-      : inputOne = inputOne
-      ;
-    
-      output.innerText = inputOne
+      : (inputOne = inputOne);
+    output.innerText = inputOne;
     console.log(inputOne);
   } else if (operatorSymbol !== undefined) {
     inputTwo === undefined
       ? (inputTwo = event.target.textContent)
       : inputTwo.length < 10
       ? (inputTwo += event.target.textContent)
-      : inputTwo = inputTwo  
-      output.innerText = inputTwo
+      : (inputTwo = inputTwo);
+    output.innerText = inputTwo;
     console.log(inputTwo);
   }
 }
@@ -236,57 +117,59 @@ document.querySelectorAll(".operator").forEach((item) => {
 function getOperator(event) {
   if (inputOne !== undefined) {
     operatorSymbol = event.target.textContent;
-  } 
-  if (operatorSymbol === undefined){
+  }
+  if (operatorSymbol === undefined) {
     operatorSymbol = event.target.textContent;
   }
-  if(inputTwo !== undefined){
-    operating()
+  if (inputTwo !== undefined) {
+    operating();
     operatorSymbol = event.target.textContent;
-
   }
   console.log(operatorSymbol);
 }
 
-document.querySelector('#percent').addEventListener('click', percent)
+document.querySelector("#percent").addEventListener("click", percent);
 
-function percent(){
-  if (inputOne !== undefined){
-    inputOne = inputOne / 100
-    output.innerText = inputOne
-  } else if (inputTwo !== undefined){
-    inputTwo = inputTwo / 100
-  } else if (sum !== undefined){
-    sum = sum / 100
+function percent() {
+  if (inputOne !== undefined) {
+    inputOne = inputOne / 100;
+    output.innerText = inputOne;
+  } else if (inputTwo !== undefined) {
+    inputTwo = inputTwo / 100;
+  } else if (sum !== undefined) {
+    sum = sum / 100;
   }
 }
 
-document.querySelector('.plusminus').addEventListener('click', positiveNegative)
+document
+  .querySelector(".plusminus")
+  .addEventListener("click", positiveNegative);
 
-function positiveNegative(){
-  if(output.innerText === inputOne){
-    output.innerText = -inputOne
-    inputOne = -inputOne
-
-  } else if (output.innerText === inputTwo){
-    output.innerText = -inputTwo
-    inputTwo = -inputTwo
-
-  } else if (output.innerText === sum){
-    output.innerText = -sum
-    sum = -sum
-
-  } 
+function positiveNegative() {
+  if (output.innerText === inputOne) {
+    output.innerText = -inputOne;
+    inputOne = -inputOne;
+  } else if (output.innerText === inputTwo) {
+    output.innerText = -inputTwo;
+    inputTwo = -inputTwo;
+  } else if (output.innerText === sum) {
+    output.innerText = -sum;
+    sum = -sum;
+  }
 }
 
 document.querySelector(".equals").addEventListener("click", operating);
 
 function operating() {
   if (sum === undefined) {
-    let calc = new Calculator(Number(inputOne), Number(inputTwo), operatorSymbol);
+    let calc = new Calculator(
+      Number(inputOne),
+      Number(inputTwo),
+      operatorSymbol
+    );
     calc.operate();
-  } else if (sum !== undefined){
-    let calc = new Calculator(Number(sum), Number(inputTwo), operatorSymbol)
-    calc.operate()
+  } else if (sum !== undefined) {
+    let calc = new Calculator(Number(sum), Number(inputTwo), operatorSymbol);
+    calc.operate();
   }
 }
